f020abaf96e91ea2bf03699d457e866076349426
sam greaves <sam.greaves@bglgroup.com>
2020-04-25 16:23:47 +0100

    Added my stocks for testing

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,16 @@
 let express = require('express');
 let bodyParser = require('body-parser');
 
+let stocks = {stocks:
+        [
+            { name:  "NewRiver REIT", symbol: "NRR", quantity: 43},
+            { name: "Pembina Pipeline", symbol: "PBA", quantity: 4},
+            { name: "Barclays", symbol: "BARC", quantity: 12},
+            { name: "Salesforce", symbol: "CRM", quantity: 0.06},
+            { name: "Tencent Music Entertainment", symbol: "TME", quantity: 1},
+        ]};
+
+
 let app = express();
 
 // Routes
